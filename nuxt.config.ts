b9a950aeccc7a2b9b4af0520074b7601f714c0b8
Tomohiro Yoshida <rollwithit0117@gmail.com>
2020-05-18 02:19:10 +0900

    [update] nuxtconfigにenvを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
 import { NuxtAxiosInstance } from '@nuxtjs/axios'
 require('dotenv').config()
 
+const env = {
+  API_KEY: process.env.API_KEY,
+  AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+  DATABASE_URL: process.env.DATABASE_URL,
+  PROJECT_ID: process.env.PROJECT_ID,
+  STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+  MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+  APP_ID: process.env.APP_ID,
+  MEASUREMENT_ID: process.env.MEASUREMENT_ID
+}
+
 export default {
+  env,
   mode: 'spa',
   server: {
     // host: '0.0.0.0' // default: localhost
